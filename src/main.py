f739ac33ff795adc295e25e97bbe34c189cc5b56
Birband <maciejsztramski@gmail.com>
2024-12-14 21:04:10 +0100

    feat: add ai_models router to main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from .routers import example
 from .routers import database_example
+from .routers import ai_models
 
 app = FastAPI(
     title='renameme',
@@ -11,3 +12,4 @@ app = FastAPI(
 
 app.include_router(example.router)
 app.include_router(database_example.router)
+app.include_router(ai_models.router)
