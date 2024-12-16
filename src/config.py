69abddea21f020e532c2b31e18fd1dda26727127
Birband <maciejsztramski@gmail.com>
2024-12-16 22:15:39 +0100

    feat: update paths and names for Settings

--- src/config.py
+++ src/config.py
@@ -1,13 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
     DATABASE_URL : str ="sqlite:///./databases/models.db"
-    MODEL_PATH : str ="./models"
+    TOKENIZER_PATH : str ="./src/tokenizers"
+    MODEL_PATH : str ="./src/models"
     DEBUG : bool = False
     # Base models 
-    ROBERTA : str = "roberta-base"
-    HERBERT : str ="herbert-base-cased"
-    XLM_ROBERTA : str ="xlm-roberta"
+    ROBERTA : str = "Jean-Baptiste/roberta-large-ner-english"
+    HERBERT : str ="pietruszkowiec/herbert-base-ner"
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
