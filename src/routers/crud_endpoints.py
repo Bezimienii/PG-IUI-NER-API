2f9187b771bb2cd5adda6e19a7f14c764c20abb1
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-01-26 10:46:59 +0100

    Fix endpoints descriptions

--- src/routers/crud_endpoints.py
+++ src/routers/crud_endpoints.py
@@ -10,15 +10,32 @@ from ..utils.crud import delete_model, get_model, get_models, get_subprocesses
 router = APIRouter(prefix='/api/model', tags=['AI Models'])
 
 
-@router.get('/', summary='Get all AI models')
+@router.get('/', summary='Fetches all AI models from the database.')
 def get_ai_models(db: Session = Depends(get_db)) -> dict:
-    """Gets all AI models.
+    """This endpoint retrieves metadata for all available AI models in the system, including their IDs, base models, file paths, creation dates, training status, and versions.
 
-    Args:
-        db (Session): The database session.
+    **Args**:
+    - **db** (Session): The database session.
 
-    Returns:
-        dict: A dictionary containing the all available AI models data.
+    **Returns**:
+    - **dict**: A dictionary containing the data of all available AI models with the following structure:
+        ```json
+            {
+                "models": [
+                    {
+                        "id": int,
+                        "base_model": str,
+                        "file_path": str,
+                        "date_created": str,
+                        "is_training": bool,
+                        "is_trained": bool,
+                        "version": int
+                    }
+                ]
+            }
+        ```
+    **Raises**:
+    - **HTTPException (404)**: If no models are found in the database.
     """
 
     models = get_models(db)
@@ -41,15 +58,29 @@ def get_ai_models(db: Session = Depends(get_db)) -> dict:
     else:
         raise HTTPException(status_code=404, detail='Model not found')
 
-@router.get('/subs', summary='Get all AI models')
+
+@router.get('/subs', summary='Fetches all AI model subprocesses stored in the database.')
 def get_ai_subs(db: Session = Depends(get_db)) -> dict:
-    """Gets all AI models.
+    """This endpoint retrieves information about all subprocesses associated with AI models, including their unique process IDs and names.
 
-    Args:
-        db (Session): The database session.
+    **Args**:
+    - **db** (Session): The database session.
 
-    Returns:
-        dict: A dictionary containing the all available AI models data.
+    **Returns**:
+    - **dict**: A dictionary with the following structure:
+        ```json
+            {
+                "models": [
+                    {
+                        "id": str,
+                        "name": str
+                    }
+                ]
+            }
+        ```
+
+    **Raises**:
+    - **HTTPException (404)**: If no subprocesses are found in the database.
     """
 
     models = get_subprocesses(db)
@@ -65,15 +96,25 @@ def get_ai_subs(db: Session = Depends(get_db)) -> dict:
     else:
         raise HTTPException(status_code=404, detail='Model not found')
 
-@router.delete('/subs/{sub}', summary='Get all AI models')
+
+@router.delete('/subs/{sub}', summary='Deletes a specific AI model subprocess by its ID.')
 def get_ai_subs(sub:int, db: Session = Depends(get_db)) -> dict:
-    """Gets all AI models.
+    """This endpoint removes an AI model subprocess from the system, identified by its unique process ID.
 
-    Args:
-        db (Session): The database session.
+    **Args**:
+    - **sub** (int): The unique ID of the subprocess to delete.
+    - **db** (Session): The database session.
 
-    Returns:
-        dict: A dictionary containing the all available AI models data.
+    **Returns**:
+    - **dict**: A message confirming the deletion of the subprocess, with the following structure:
+        ```json
+            {
+                "message": "Subprocess with ID {sub} deleted successfully"
+            }
+        ```
+
+    **Raises**:
+    - **HTTPException (404)**: If the subprocess with the specified ID is not found in the database.
     """
 
     models = delete_sub(sub, db)
@@ -83,18 +124,31 @@ def get_ai_subs(sub:int, db: Session = Depends(get_db)) -> dict:
         raise HTTPException(status_code=404, detail='Model not found')
 
 
-
-
-@router.get('/{model_id}', summary='Get an AI model by ID')
+@router.get('/{model_id}', summary='Fetches a specific AI model by its unique ID.')
 def get_ai_model(model_id: int, db: Session = Depends(get_db)) -> dict:
-    """Gets an AI model by its ID.
+    """This endpoint retrieves detailed information about an individual AI model, including its metadata such as ID, name, base model, file path, creation date, training status, and version.
 
-    Args:
-        model_id (int): The ID of the AI model to get.
-        db (Session): The database session.
+    **Args**:
+    - **model_id** (int): The unique ID of the AI model to retrieve.
+    - **db** (Session): The database session.
 
-    Returns:
-        dict: A dictionary containing the AI model data.
+    **Returns**:
+    - **dict**: A dictionary containing the data of the specified AI model, with the following structure:
+        ```json
+            {
+                "id": int,
+                "model_name": str,
+                "base_model": str,
+                "file_path": str,
+                "date_created": str,
+                "is_training": bool,
+                "is_trained": bool,
+                "version": int
+            }
+        ```
+
+    **Raises**:
+    - **HTTPException (404)**: If the model with the specified ID is not found in the database.
     """
 
     model = get_model(db, model_id)
@@ -113,16 +167,24 @@ def get_ai_model(model_id: int, db: Session = Depends(get_db)) -> dict:
         raise HTTPException(status_code=404, detail='Model not found')
 
 
-@router.delete('/{model_id}', summary='Delete an AI model by ID')
+@router.delete('/{model_id}', summary='Deletes a specific AI model by its unique ID.')
 def delete_ai_model(model_id: int, db: Session = Depends(get_db)) -> dict:
-    """Deletes an AI model by its ID.
+    """This endpoint removes an AI model from the system, identified by its unique ID.
 
-    Args:
-        model_id (int): The ID of the AI model to delete.
-        db (Session): The database session.
+    **Args**:
+    - **model_id** (int): The unique ID of the AI model to delete.
+    - **db** (Session): The database session used to perform the deletion.
 
-    Returns:
-        dict: A dictionary containing the message of the deletion.
+    **Returns**:
+    - **dict**: A message confirming the deletion of the AI model, with the following structure:
+        ```json
+            {
+                "message": "Model with ID {model_id} deleted successfully"
+            }
+        ```
+
+    **Raises**:
+    - **HTTPException (404)**: If the model with the specified ID is not found in the database.
     """
 
     model = delete_model(db, model_id)
